1710c3221c8db2a0c2dd8b485a7151f4d49a45a3
Thomas Twiton <twitth01@luther.edu>
2018-11-11 13:50:42 -0800

    Mostly working, server bugs out after a couple requests

--- project4/webserver.py
+++ project4/webserver.py
@@ -12,8 +12,64 @@ LOGFILE = "webserver.log"
 
 def main():
     """Main loop"""
-    pass
+    #create server socket
+    server.bind((ADDRESS, PORT))
+    server.listen(1)
+    while True:
+        #get the info
+        conn, addr = server.accept()
+        client_address = addr[0]
+        data = conn.recvfrom(1024)
 
+        #process the info
+        #first line
+        datalist = str(data[0]).split('\\r\\n')
+        first_line = datalist[0][2:]
+        print(first_line)
+        first_line = first_line.split()
+        http_type = first_line[0]
+        file_req =  first_line[1]
+        version = first_line[2]
+
+        #headers
+        headers = {}
+        for i in range(1, len(datalist)):
+            keyval = datalist[i].split(':')
+            if len(keyval) >= 2: #the last couple lines are junk
+                headers[keyval[0]] = keyval[1]
+
+        #record to log file
+        log = open(LOGFILE, 'a')
+        log.write(str(datetime.now())+' | '+file_req+' | '+client_address+' | '+headers['User-Agent']+'\n')
+        log.close()
+
+        #if not a GET request, we don't know what to do
+        if http_type != "GET":
+            #return 405
+            conn.sendall("HTTP/1.1 405 Method Not Allowed \r\n \r\n".encode())
+        elif file_req != "/alice30.txt":
+            #return 404
+            conn.sendall("HTTP/1.1 404 Not Found \r\n \r\n".encode())
+        else:
+            #return the text file
+            alice_file =  open("alice30.txt", 'r')
+            alice = alice_file.read()  
+
+            response = "HTTP/1.1 200 OK \r\n"
+            response += ("Content-Length:" + str(len(alice)) + '\r\n')
+            response += ("Content-Type: text/plain; charset=utf-8 \r\n")
+            response += ("Date:" + str(datetime.now()) + '\r\n')
+            response += ("Last-Modified: Wed Aug 29 11:00:00 2018 \r\n")
+            response += ("Server: CS430-ROMAN \r\n")
+            response += ("\r\n")
+            response += alice
+            conn.sendall(response.encode())
+        conn.close()
+
+
+
+    server.close()
+    print("We did it")
 
 if __name__ == "__main__":
     main()
