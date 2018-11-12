2fbd3647e704453e209110413c3c41273b1d1346
Thomas Twiton <twitth01@luther.edu>
2018-11-12 09:09:01 -0800

    Slight improvements

--- project4/webserver.py
+++ project4/webserver.py
@@ -24,48 +24,51 @@ def main():
         #process the info
         #first line
         datalist = str(data[0]).split('\\r\\n')
-        first_line = datalist[0][2:]
-        print(datalist)
-        print(first_line) 
-        first_line = first_line.split()
-        http_type = first_line[0]
-        file_req =  first_line[1]
-        version = first_line[2]
-
-        #headers
-        headers = {}
-        for i in range(1, len(datalist)):
-            keyval = datalist[i].split(':')
-            if len(keyval) >= 2: #the last couple lines are junk
-                headers[keyval[0]] = keyval[1]
-
-        #record to log file
-        log = open(LOGFILE, 'a')
-        log.write(str(datetime.now())+' | '+file_req+' | '+client_address+' | '+headers['User-Agent']+'\n')
-        log.close()
-
-        #if not a GET request, we don't know what to do
-        if http_type != "GET":
-            #return 405
-            conn.sendall("HTTP/1.1 405 Method Not Allowed \r\n \r\n".encode())
-        elif file_req != "/alice30.txt":
-            #return 404
+        if len(datalist) < 4: #sometimes Chrome sends me an empty string (b'')
             conn.sendall("HTTP/1.1 404 Not Found \r\n \r\n".encode())
         else:
-            #return the text file
-            alice_file =  open("alice30.txt", 'r')
-            alice = alice_file.read()  
+            first_line = datalist[0][2:]
+            #print(datalist)
+            print(first_line) 
+            first_line = first_line.split()
+            http_type = first_line[ 0]
+            file_req =  first_line[1]
+            version = first_line[2]
 
-            response = "HTTP/1.1 200 OK \r\n"
-            response += ("Content-Length:" + str(len(alice)) + '\r\n')
-            response += ("Content-Type: text/plain; charset=utf-8 \r\n")
-            response += ("Date:" + str(datetime.now()) + '\r\n')
-            response += ("Last-Modified: Wed Aug 29 11:00:00 2018 \r\n")
-            response += ("Server: CS430-ROMAN \r\n")
-            response += ("\r\n\r\n")
-            response += alice
-            conn.sendall(response.encode())
-        
+            #headers
+            headers = {}
+            for i in range(1, len(datalist)):
+                keyval = datalist[i].split(':')
+                if len(keyval) >= 2: #the last couple lines are junk
+                    headers[keyval[0]] = keyval[1]
+
+            #record to log file
+            log = open(LOGFILE, 'a')
+            log.write(str(datetime.now())+' | '+file_req+' | '+client_address+' | '+headers['User-Agent']+'\n')
+            log.close()
+
+            #if not a GET request, we don't know what to do
+            if http_type != "GET":
+                #return 405
+                conn.sendall("HTTP/1.1 405 Method Not Allowed \r\n".encode())
+            elif file_req != "/alice30.txt":
+                #return 404
+                conn.sendall("HTTP/1.1 404 Not Found \r\n".encode())
+            else:
+                #return the text file
+                alice_file =  open("alice30.txt", 'r')
+                alice = alice_file.read()  
+
+                response = "HTTP/1.1 200 OK \r\n"
+                response += ("Content-Length:" + str(len(alice)) + '\r\n')
+                response += ("Content-Type: text/plain; charset=utf-8 \r\n")
+                response += ("Date:" + str(datetime.now()) + '\r\n')
+                response += ("Last-Modified: Wed Aug 29 11:00:00 2018 \r\n")
+                response += ("Server: CS430-THOMAS \r\n")
+                response += ("\r\n\r\n")
+                response += alice
+                conn.sendall(response.encode())
+        conn.close()
 
     server.close()
     print("We did it")
