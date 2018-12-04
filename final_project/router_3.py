eef2deabb90e20fe4029411b4d085ce001171bce
Thomas Twiton <twitth01@luther.edu>
2018-12-03 21:48:20 -0800

    Read config, format update

--- final_project/router_3.py
+++ final_project/router_3.py
@@ -6,7 +6,7 @@
 import os
 import random
 import select
-from socket import socket, AF_INET, SOCK_DGRAM
+import socket
 import struct
 import sys
 import time
@@ -25,24 +25,8 @@ MESSAGES = [
     "Precise Pangolin"
 ]
 
-def val_to_bytes(value: int, n_bytes: int) -> list:
-    '''Split a value into n bytes'''
-    bytelist = [None] * n_bytes
-    for i in range(0, n_bytes):
-        bytelist[n_bytes - 1 - i] = (value >> (8*i)) & 0xFF
-    return bytelist
-
-
-def bytes_to_val(bytes_lst: list) -> int:
-    '''Merge n bytes into a value'''
-    sum = 0
-    for i in range(0, len(bytes_lst)):
-        sum += bytes_lst[len(bytes_lst) - 1 - i] << (8*i)
-    return sum
-
 def read_file(filename: str) -> None:
     """Read config file"""
-    #print(filename)
     f = open(filename, 'r')
     #print(THIS_NODE)
 
@@ -57,33 +41,21 @@ def read_file(filename: str) -> None:
                 linelist = line.split()
                 #print(linelist)
                 NEIGHBORS.add(linelist[0])
-                ROUTING_TABLE[linelist[0]] = [int(linelist[1]), linelist[0]]
+                ROUTING_TABLE[linelist[0]] = [int(linelist[1]), THIS_NODE]
                 line= f.readline()
             #print("OH WE DUN NKOW")
         else:
             line = f.readline()
     
-    #print(NEIGHBORS)
-    #print(ROUTING_TABLE)
+    print(NEIGHBORS)
+    print(ROUTING_TABLE)
 
     f.close()
 
 def format_update():
     """Format update message"""
-    update = bytearray()
-    update.extend([0])
+    raise NotImplementedError
 
-    for route in ROUTING_TABLE:
-        address = route.split(".")
-        #print(address)
-        for byte in address:
-            update.extend([int(byte)])
-        
-        cost = ROUTING_TABLE[route][0]
-        update.extend([cost])
-
-    #print(update)
-    return update
 
 def parse_update(msg, neigh_addr):
     """Update routing table"""
@@ -92,8 +64,7 @@ def parse_update(msg, neigh_addr):
 
 def send_update(node):
     """Send update"""
-    update = format_update()
-    router.sendto(update, node)
+    raise NotImplementedError
 
 
 def format_hello(msg_txt, src_node, dst_node):
@@ -113,31 +84,12 @@ def send_hello(msg_txt, src_node, dst_node):
 
 def print_status():
     """Print status"""
-    print("\t Host \t \t Cost \t Via")
-    for route in ROUTING_TABLE:
-        print("\t", route,  "\t", ROUTING_TABLE[route][0], "\t", ROUTING_TABLE[route][1])
-
+    raise NotImplementedError
 
 
 def main(args: list):
     """Router main loop"""
-    try:
-        read_file(args[1])
-    except:
-        print(f"Usage: {args[0]} <configfile>")
-
-    router = socket(AF_INET, SOCK_DGRAM)
-    print(time.ctime()[-13:-5], "| Router", THIS_NODE , " here")
-    RPORT = PORT + int(HOST_ID)
-    print(time.ctime()[-13:-5], "| Binding to", THIS_NODE+':'+str(RPORT))
-    router.bind((THIS_NODE, RPORT))    
-    print(time.ctime()[-13:-5], "| Listening on", THIS_NODE+':'+str(RPORT))
-
-    print_status()
-
-    update = format_update()
-
-    router.close()
+    read_file("network_1_config.txt")
 
 
 if __name__ == "__main__":
